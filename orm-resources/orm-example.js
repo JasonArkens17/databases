9ec5e2413c9971231454925ddd3b8d66304300a0
Jason Davenport <jason.arkens17@gmail.com>
2016-09-15 14:48:27 -0700

    moving to new repo

--- orm-resources/orm-example.js
+++ orm-resources/orm-example.js
@@ -5,11 +5,7 @@
 
 var Sequelize = require('sequelize');
 var db = new Sequelize('chatter', 'root', '');
-/* TODO this constructor takes the database name, username, then password.
- * Modify the arguments if you need to */
 
-/* first define the data structure by giving property names and datatypes
- * See http://sequelizejs.com for other datatypes you can use besides STRING. */
 var User = db.define('User', {
   username: Sequelize.STRING
 });
@@ -20,15 +16,11 @@ var Message = db.define('Message', {
   roomname: Sequelize.STRING
 });
 
-/* Sequelize comes with built in support for promises
- * making it easy to chain asynchronous operations together */
 User.sync()
   .then(function() {
-    // Now instantiate an object and save it:
     return User.create({username: 'Jean Valjean'});
   })
   .then(function() {
-    // Retrieve objects from the database:
     return User.findAll({ where: {username: 'Jean Valjean'} });
   })
   .then(function(users) {
@@ -38,7 +30,6 @@ User.sync()
     db.close();
   })
   .catch(function(err) {
-    // Handle any error in the chain
     console.error(err);
     db.close();
   });
